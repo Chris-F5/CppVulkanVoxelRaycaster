ab07bd38361778ea9eb1670490c218e09e96f710
Chris-F5 <christopher-d-lang@outlook.com>
2021-04-26 17:57:02 +0100

    Refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtx/quaternion.hpp>
 
-#include <iostream>
-#include <fstream>
-#include <stdexcept>
-#include <cstdlib>
-#include <vector>
-#include <cstring>
-#include <optional>
-#include <set>
+#include <string>
 
 #include "vulkan_device.hpp"
 #include "render_pipeline.hpp"
@@ -24,6 +17,15 @@ const bool enableValidationLayers = true;
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+GLFWwindow *createWindow(std::string title, uint32_t width, uint32_t height)
+{
+    // Dont use opengl
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    return glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
+}
 
 bool rightArrowDown = false;
 bool leftArrowDown = false;
@@ -70,125 +72,91 @@ void keyCallback(GLFWwindow *_window, int key, int _scanCode, int action, int _m
     }
 }
 
-class App
+void mainLoop(GLFWwindow *window, RenderPipeline *renderPipeline)
 {
-public:
-    void run()
-    {
-        initWindow();
-        renderPipeline = createRenderPipeline(window, enableValidationLayers, "shader.spv");
-        mainLoop();
-        cleanup();
-    }
-
-private:
-    GLFWwindow *window;
-    RenderPipeline renderPipeline;
     glm::vec4 cameraPosition = glm::vec4(0.0);
     glm::vec2 cameraRot = glm::vec2(0.0);
 
-    void initWindow()
+    while (!glfwWindowShouldClose(window))
     {
-        glfwInit();
+        glfwPollEvents();
 
-        // Dont use opengl
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulcan Test App", nullptr, nullptr);
-
-        glfwSetKeyCallback(window, keyCallback);
-    }
-    void mainLoop()
-    {
-        while (!glfwWindowShouldClose(window))
+        if (dKeyDown)
         {
-            glfwPollEvents();
-
-            if (dKeyDown)
-            {
-                cameraPosition.x += 0.01;
-            }
-            if (aKeyDown)
-            {
-                cameraPosition.x -= 0.01;
-            }
-            if (wKeyDown)
-            {
-                cameraPosition.y += 0.01;
-            }
-            if (sKeyDown)
-            {
-                cameraPosition.y -= 0.01;
-            }
-
-            if (rightArrowDown)
-            {
-                cameraRot.y += 0.1;
-            }
-            if (leftArrowDown)
-            {
-                cameraRot.y -= 0.1;
-            }
-            if (upArrowDown)
-            {
-                cameraRot.x -= 0.1;
-            }
-            if (downArrowDown)
-            {
-                cameraRot.x += 0.1;
-            }
-
-            UniformData camInfo;
-            camInfo.camPos = cameraPosition;
-            // ======== MAKE ROT MATRIX
-            float pi = 3.141592;
-            // camRotX and Y are defined elsewhere and can be controlled from the keyboard during runtime.
-            glm::vec3 camEulerAngles = glm::vec3(cameraRot.x, cameraRot.y, 0);
-
-            // Convert to radians
-            camEulerAngles.x = camEulerAngles.x * pi / 180;
-            camEulerAngles.y = camEulerAngles.y * pi / 180;
-            camEulerAngles.z = camEulerAngles.z * pi / 180;
-
-            // Generate Quaternian
-            glm::quat camRotation;
-            camRotation = glm::quat(camEulerAngles);
-
-            // Generate rotation matrix from quaternian
-            glm::mat4 camToWorldMatrix = glm::toMat4(camRotation);
-            // =======
-            camInfo.camRotMat = camToWorldMatrix;
-
-            drawFrame(&renderPipeline, camInfo);
-
-            //std::cout << cameraPosition.x << " " << cameraPosition.y << std::endl;
+            cameraPosition.x += 0.01;
+        }
+        if (aKeyDown)
+        {
+            cameraPosition.x -= 0.01;
+        }
+        if (wKeyDown)
+        {
+            cameraPosition.y += 0.01;
+        }
+        if (sKeyDown)
+        {
+            cameraPosition.y -= 0.01;
         }
 
-        vkDeviceWaitIdle(renderPipeline.device);
-    }
+        if (rightArrowDown)
+        {
+            cameraRot.y += 0.1;
+        }
+        if (leftArrowDown)
+        {
+            cameraRot.y -= 0.1;
+        }
+        if (upArrowDown)
+        {
+            cameraRot.x -= 0.1;
+        }
+        if (downArrowDown)
+        {
+            cameraRot.x += 0.1;
+        }
 
-    void cleanup()
-    {
-        cleanupRenderPipeline(&renderPipeline);
-        glfwDestroyWindow(window);
-        glfwTerminate();
+        UniformData camInfo;
+        camInfo.camPos = cameraPosition;
+        // ======== MAKE ROT MATRIX
+        float pi = 3.141592;
+        // camRotX and Y are defined elsewhere and can be controlled from the keyboard during runtime.
+        glm::vec3 camEulerAngles = glm::vec3(cameraRot.x, cameraRot.y, 0);
+
+        // Convert to radians
+        camEulerAngles.x = camEulerAngles.x * pi / 180;
+        camEulerAngles.y = camEulerAngles.y * pi / 180;
+        camEulerAngles.z = camEulerAngles.z * pi / 180;
+
+        // Generate Quaternian
+        glm::quat camRotation;
+        camRotation = glm::quat(camEulerAngles);
+
+        // Generate rotation matrix from quaternian
+        glm::mat4 camToWorldMatrix = glm::toMat4(camRotation);
+        // =======
+        camInfo.camRotMat = camToWorldMatrix;
+
+        drawFrame(renderPipeline, camInfo);
+
+        //std::cout << cameraPosition.x << " " << cameraPosition.y << std::endl;
     }
-};
+}
 
 int main()
 {
-    App app;
+    glfwInit();
+    GLFWwindow *window = createWindow("Vulkan Test App", WIDTH, HEIGHT);
+    glfwSetKeyCallback(window, keyCallback);
 
-    try
-    {
-        app.run();
-    }
-    catch (const std::exception &err)
-    {
-        std::cerr << err.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+    RenderPipeline renderPipeline = createRenderPipeline(window, enableValidationLayers, "shader.spv");
+
+    mainLoop(window, &renderPipeline);
+
+    vkDeviceWaitIdle(renderPipeline.device);
+
+    cleanupRenderPipeline(&renderPipeline);
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return EXIT_SUCCESS;
 }
